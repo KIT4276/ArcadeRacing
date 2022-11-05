59eae7f1f35a1d8e9661640d3c27d0b32391d81c
KIT4276 <kit311290@gmail.com>
2022-11-05 12:25:45 +0300

    Start creating a bot

--- Assets/Scripts/BotInput.cs
+++ Assets/Scripts/BotInput.cs
@@ -4,8 +4,40 @@ using UnityEngine;
 
 namespace Racing
 {
-    public class BotInput : MonoBehaviour
+    public class BotInput : BaseInput
     {
+        private int _index;
 
+        [SerializeField]
+        private BotTargetPoint[] _points;
+
+        private void Start()
+        {
+            GetAngle();
+        }
+        protected override void FixedUpdate()
+        {
+            Acceleration = 1f;
+        }
+
+        private float GetAngle()
+        {
+            var targetPos = _points[_index].transform.position;
+            Debug.Log(_points[_index].transform.name);
+            targetPos.y = transform.position.y;
+
+            var direction = targetPos - transform.position;
+
+            return Vector3.SignedAngle(direction, transform.forward, transform.up);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.GetComponent<BotTargetPoint>() != null)
+            {
+                _index++;
+                GetAngle();
+            }
+        }
     }
 }
---
+++ Assets/Scripts/BotTargetPoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Racing
+{
+    public class BotTargetPoint : MonoBehaviour
+    {
+       
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(transform.position, 5f);
+        }
+    }
+}
