34398413505b3109dd8207fdd19d3f9bb68ef0ab
KIT4276 <kit311290@gmail.com>
2022-11-08 19:16:10 +0300

    Done: Menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Racing
+{
+    public class Menu : MonoBehaviour
+    {
+        public void StartRace()
+            =>SceneManager.LoadScene(1);
+
+        public void StartTuning()
+            => SceneManager.LoadScene(2);
+
+        public void Exit()
+        {
+#if UNITY_EDITOR           
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
+            Application.Quit();
+        }
+    }
+}
