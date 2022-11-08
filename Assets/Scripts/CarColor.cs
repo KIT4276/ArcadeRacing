8909d055044eca1018d85de7d2f5f72467872a14
KIT4276 <kit311290@gmail.com>
2022-11-08 20:49:38 +0300

    Started: Tuning: color

---
+++ Assets/Scripts/CarColor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Racing
+{
+    public class CarColor : MonoBehaviour
+    {
+        [SerializeField]
+        private Material _material1;
+        [SerializeField]
+        private Material _material2;
+        [SerializeField]
+        private Material _material3;
+        [SerializeField]
+        private Material _material4;
+        [SerializeField]
+        private Material _material5;
+
+        public void CangeMaterial1()
+            =>GetComponent<MeshRenderer>().material = _material1;
+        public void CangeMaterial2()
+            => GetComponent<MeshRenderer>().material = _material2;
+        public void CangeMaterial3()
+            => GetComponent<MeshRenderer>().material = _material3;
+        public void CangeMaterial4()
+            => GetComponent<MeshRenderer>().material = _material4;
+        public void CangeMaterial5()
+            => GetComponent<MeshRenderer>().material = _material5;
+
+
+    }
+}
---
+++ Assets/Scripts/TuningCamera.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Racing
+{
+    public class TuningCamera : MonoBehaviour
+    {
+        [SerializeField]
+        private float _rotateSpeed;
+
+        [SerializeField]
+        private Button _buttonRight;
+        [SerializeField]
+        private Button _buttonLeft;
+
+        public void RightRotate()
+            => Rotate(true);
+
+
+        public void LeftRotate()
+            => Rotate(false);
+
+        public void Rotate(bool right)
+        {
+            var angle = transform.eulerAngles;
+
+            angle.x = 0f;
+            if (right) 
+                angle.y += _rotateSpeed;
+            else 
+                angle.y -= _rotateSpeed;
+            angle.z = 0f;
+
+            transform.eulerAngles = angle;
+        }
+    }
+}
